cad901e009ff9bd76403b7ad7336a5fe285bff77
Stephanie Legarra <stephanielegarra@gmail.com>
2023-09-01 01:39:58 -0300

    cart

---
+++ src/public/js/product.js
@@ -0,0 +1,19 @@
+const addToCartButton = document.querySelector(".card button#addToCart");
+
+addToCartButton.addEventListener("click", () => {
+  const card = document.querySelector(".card");
+  const idx = card.querySelector(".card_id").textContent;
+  console.log(id);
+
+  // Deja el carrito 1 predeterminado
+  fetch(`/api/cart/1/products/${id}`, {
+    method: "POST",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
